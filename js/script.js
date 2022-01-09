9665d1659dd98dc034162d309996bad6d755f494
EduP217 <prieto_217@hotmail.com>
2022-01-08 19:14:06 -0500

    update document lastModified

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let currentDate = new Date();
 let currentYear = currentDate.getFullYear();
-let lastupdateDateTime = `${(currentDate.getMonth()+1)}/${currentDate.getDate()}/${currentDate.getFullYear()} ${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}`;
+//let lastupdateDateTime = `${(currentDate.getMonth()+1)}/${currentDate.getDate()}/${currentDate.getFullYear()} ${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}`;
+let lastupdateDateTime = document.lastModified;
 
 document.getElementById('idCurrentYear').innerHTML = currentYear;
 document.getElementById('idLastupdate').innerHTML = lastupdateDateTime;
