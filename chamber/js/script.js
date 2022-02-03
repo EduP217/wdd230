2789385b2590cbc701165cc3afad6ce19337266f
EduP217 <prieto_217@hotmail.com>
2022-02-03 11:45:59 -0500

    project - chamber of commerce

---
+++ chamber/js/script.js
@@ -0,0 +1,67 @@
+let months = new Array("January","February","March","April","May","June","July","August","September","October","November","December");
+let weekDay = new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday");
+
+let currentDate = new Date();
+let currentYear = currentDate.getFullYear();
+let today = `${weekDay[currentDate.getDay()]}, ${currentDate.getDate()} ${months[currentDate.getMonth()]} ${currentYear}`;
+let lastupdateDateTime = document.lastModified;
+
+document.getElementById('titleCurrentDate').innerHTML = today;
+//document.getElementById('footerCurrentYear').innerHTML = currentYear;
+//document.getElementById('footerLastModified').innerHTML = `Last Modification: ${lastupdateDateTime}`;
+
+const recaulculateResponsive = () => {
+
+    let isMobile = false;
+    let isTablet = false;
+    let isDesktop = false;
+
+    let windowsWidth = window.screen.width;
+    if(windowsWidth >= 1024){
+        isMobile = false;
+        isTablet = false;
+        isDesktop = true;
+    } else if(windowsWidth >= 768){
+        isMobile = false;
+        isTablet = true;
+        isDesktop = false;
+    } else {
+        isMobile = true;
+        isTablet = false;
+        isDesktop = false;
+    }
+    
+    let userInfo = ["@%s Leoncio Prado Chamber", "<b>Eduardo Prieto</b>", "WDD 230 Project", "Last Modification: %s"];
+    let userInfoMessage = "";
+    for(let i=0; i<userInfo.length; i++){
+        if(i==0){
+            let textreplaced = userInfo[i].replace('%s',currentYear);
+            userInfoMessage += textreplaced;
+        } else if(i == (userInfo.length-1)){
+            let textreplaced = userInfo[i].replace('%s',lastupdateDateTime);
+            userInfoMessage += textreplaced;
+        } else {
+            userInfoMessage += userInfo[i];
+        }
+
+        if(isMobile){
+            userInfoMessage += " <br> ";
+        } else {
+            userInfoMessage += " | ";
+        }
+    }
+    document.getElementById('userInfo').innerHTML = userInfoMessage;
+}
+
+recaulculateResponsive();
+window.onresize = () => {
+    console.log("windows on resize");
+    recaulculateResponsive();
+};
+
+const toggleMenu = () => {
+    document.querySelector('#menu-ham').classList.toggle('active');
+    document.querySelector('#siteMenu').classList.toggle('open');
+}
+
+document.querySelector('#menu-ham').addEventListener('click', toggleMenu);
