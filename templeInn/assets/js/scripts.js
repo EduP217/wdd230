1c168cb01222b067ffc23b08720a2ef6ffdee10e
EduP217 <prieto_217@hotmail.com>
2022-04-06 17:43:12 -0500

    templeInn updates

--- templeInn/assets/js/scripts.js
+++ templeInn/assets/js/scripts.js
@@ -1,5 +1,6 @@
-fetch("./pages/header.html")
-//fetch("https://edup217.github.io/wdd230/templeInn/pages/header.html")
+/*
+//fetch("./pages/header.html")
+fetch("https://edup217.github.io/wdd230/templeInn/pages/header.html")
 .then(response => {
     return response.text()
 })
@@ -7,11 +8,63 @@ fetch("./pages/header.html")
     document.querySelector("header").innerHTML = data;
 });
 
-fetch("./pages/footer.html")
-//fetch("https://edup217.github.io/wdd230/templeInn/pages/footer.html")
+//fetch("./pages/footer.html")
+fetch("https://edup217.github.io/wdd230/templeInn/pages/footer.html")
 .then(response => {
     return response.text()
 })
 .then(data => {
     document.querySelector("footer").innerHTML = data;
+});
+*/
+
+let months = new Array("January","February","March","April","May","June","July","August","September","October","November","December");
+let weekDay = new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday");
+
+let currentDate = new Date();
+let currentYear = currentDate.getFullYear();
+let today = `${weekDay[currentDate.getDay()]}, ${currentDate.getDate()} ${months[currentDate.getMonth()]} ${currentYear}`;
+let lastupdateDateTime = document.lastModified;
+
+let isMobile = false;
+let isTablet = false;
+let isDesktop = false;
+
+const recaulculateResponsive = () => {
+    let windowsWidth = window.screen.width;
+    if(windowsWidth >= 1024){
+        isMobile = false;
+        isTablet = false;
+        isDesktop = true;
+    } else if(windowsWidth >= 768){
+        isMobile = false;
+        isTablet = true;
+        isDesktop = false;
+    } else {
+        isMobile = true;
+        isTablet = false;
+        isDesktop = false;
+    }
+    
+    document.getElementById('currentYear').innerHTML = currentYear;
+    document.getElementById('studentName').innerHTML = "Eduardo Prieto";
+    document.getElementById('lastModified').innerHTML = `Last Modification: ${lastupdateDateTime}`;
+}
+
+recaulculateResponsive();
+window.onresize = () => {
+    console.log("windows on resize");
+    recaulculateResponsive();
+};
+
+const toggleMenu = () => {
+    document.querySelector('#menu-ham').classList.toggle('active');
+    document.querySelector('#siteMenu').classList.toggle('open');
+    document.querySelector('#social-buttons').classList.toggle('open');
+}
+
+document.querySelector('#menu-ham').addEventListener('click', toggleMenu);
+document.querySelector('.dropdown').addEventListener('click', event => {
+    document.querySelector('.dropdown').classList.toggle('active');
+    document.querySelector('#siteSubmenu').classList.toggle('show');
 });
